ef41582971472949c210ad02d90365665a93a55d
Sinclert <Sinclert@hotmail.com>
2015-02-21 19:03:21 +0100

    Ejercicio 3

--- myls.c
+++ myls.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
 #include <dirent.h>
-#include <string.h>
+#include <sys/types.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
+
 	/* If less than two arguments (argv[0] -> program, argv[1] -> directory to search) print an error y return -1 */
-	if(argc < 2)
-	{
+	if (argc < 2){
 		printf("Too few arguments\n");
-        	return -1;
+        return -1;
 	}
 
-	/* Include your code from here */
+	// Here it is check if the directory can be open
+	DIR * myDirectory = opendir(argv[1]);
+	if (myDirectory == NULL){
+		printf("Cannot open directory %s\n", argv[1]);
+		return -1;
+	}
 
+	struct dirent * entry = readdir(myDirectory);
+
+	// While "entry" is not null, we print the directory/files names and update "entry"
+	while (entry != NULL){
+		printf("%s\n", entry -> d_name);
+		entry = readdir(myDirectory);
+	}
+
+	closedir(myDirectory);
 	return 0;
 }
